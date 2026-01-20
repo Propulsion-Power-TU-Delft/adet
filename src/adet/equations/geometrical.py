Restore hecc example, fix endwall formula

src/adet/equations/geometrical.py
@@ -21,9 +21,9 @@ class MeridionalVariable(MeridionalGeom):
         self,
         geo_rr0,
         geo_hh0,
-        geo_rr_midspan0,
         geo_area0,
         geo_height0,
+        geo_rr_midspan0,
         geo_meridional_angle0,
     ):
         _tip_radius = geo_rr_midspan0 - (geo_height0 - geo_hh0[-1]) / 2 * np.cos(
@@ -92,12 +92,12 @@ class EndwallProperties(EquationBase):
         kin_omega0,
         stc_speed_sound0,
         # Scalars
+        geo_rr_hub0,
+        geo_rr_tip0,
         kin_W_hub0,
         kin_W_tip0,
         kin_relmach_hub0,
         kin_relmach_tip0,
-        geo_rr_hub0,
-        geo_rr_tip0,
         kin_beta_hub0,
         kin_beta_tip0,
     ):
@@ -107,25 +107,26 @@ class EndwallProperties(EquationBase):
         else:
             midspan = num_span // 2
 
+        # Auxiliary variables
         delta_radius = geo_height0 / 2 * np.cos(geo_meridional_angle0)
-
         r_hub = geo_rr_midspan0 - delta_radius
         r_tip = geo_rr_midspan0 + delta_radius
+        Wt_hub = kin_Vt0[midspan] - kin_omega0 * r_hub
+        Wt_tip = kin_Vt0[midspan] - kin_omega0 * r_tip
 
+        # Residual Equations
         r1 = geo_rr_hub0 - r_hub
         r2 = geo_rr_tip0 - r_tip
-
-        Wt_hub = kin_Vt0 - kin_omega0 * r_hub
-        Wt_tip = kin_Vt0 - kin_omega0 * r_tip
 
         r3 = kin_W_hub0 - (kin_Wm0[midspan] ** 2 + Wt_hub**2) ** 0.5
         r4 = kin_W_tip0 - (kin_Wm0[midspan] ** 2 + Wt_tip**2) ** 0.5
 
-        r5 = kin_beta_hub0 - np.atan2(Wt_hub, kin_Wm0)
-        r6 = kin_beta_tip0 - np.atan2(Wt_tip, kin_Wm0)
+        r5 = kin_beta_hub0 - np.atan2(Wt_hub, kin_Wm0[midspan])
+        r6 = kin_beta_tip0 - np.atan2(Wt_tip, kin_Wm0[midspan])
 
-        r7 = stc_speed_sound0 * kin_relmach_hub0 - kin_W_hub0
-        r8 = stc_speed_sound0 * kin_relmach_tip0 - kin_W_tip0
+        # Use closest streamline for speed out sound
+        r7 = stc_speed_sound0[0] * kin_relmach_hub0 - kin_W_hub0
+        r8 = stc_speed_sound0[-1] * kin_relmach_tip0 - kin_W_tip0
 
         return r1, r2, r3, r4, r5, r6, r7, r8
 