03e1ba9b635bcc5ec0b2958b6d0271642ecfe381
fvaccari <f.vaccari@tudelft.nl>
2026-02-05 21:18:59 +0100

    Add logo

--- src/adet/tools/printing.py
+++ src/adet/tools/printing.py
@@ -1,11 +1,22 @@
+from pathlib import Path
 from art import tprint
 from datetime import datetime
+
+FOOTER = f"""
+| ~ | Developed and maintained by Francesco Vaccari
+| ~ | Propulsion & Power, Faculty of Aerospace Engineering, TU Delft
+| ~ | 2024-{datetime.now().year}
+"""
 
 
 def print_header():
     tprint('ADeT', font='isometric1')
-    print(
-        f'| ~ | Developed and maintained by Francesco Vaccari\n'
-        f'| ~ | Propulsion & Power, Faculty of Aerospace Engineering, TU Delft\n'
-        f'| ~ | 2024-{datetime.now().year}\n'
-    )
+    print(FOOTER)
+
+
+def print_logo():
+    logo_file = Path(__file__).parents[3] / 'docs/logo/ascii_logo'
+    with open(logo_file) as file:
+        logo = file.read()
+    print(logo)
+    print(FOOTER)
